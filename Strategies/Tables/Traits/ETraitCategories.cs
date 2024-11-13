710b1c002bc2e05dfd77790db908cbcd07739311
JirreV <jirreamon@gmail.com>
2024-11-13 12:43:48 +0100

    [+] Add Trait Category Enums

---
+++ Strategies/Tables/Traits/ETraitCategories.cs
@@ -0,0 +1,19 @@
+﻿using ChroniclesExporter.IO.Database;
+
+namespace ChroniclesExporter.Strategy.Traits;
+
+[DbEnum("traitCategories")]
+public enum ETraitCategories
+{
+    Condition,
+    Language,
+    MovementType,
+    Race_CreatureType,
+    Race_Size,
+    Senses,
+    Skill,
+    Spell_Component,
+    Spell_Property,
+    Spell_School,
+    Weapon_Property
+}
