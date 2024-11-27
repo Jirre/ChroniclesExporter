e6402182b21dd072db9b2b5b47d56120ebaa5068
Jirrev <jirreamon@gmail.com>
2024-11-27 23:15:08 +0100

    [+] Implement Shield Traits

---
+++ Strategies/Tables/Equipment/Shield/Links/Shield_Traits.cs
@@ -0,0 +1,12 @@
+﻿using ChroniclesExporter.IO.Database;
+
+namespace ChroniclesExporter.Strategy.Armor;
+
+// ReSharper disable once InconsistentNaming
+public class Shield_Traits : DbLinkWriter
+{
+    protected override ELink LinkId => ELink.ShieldTraits;
+
+    protected override string TableName => "shields_traits";
+    protected override string[] Fields => new[] {"shield_id", "trait_id"};
+}
