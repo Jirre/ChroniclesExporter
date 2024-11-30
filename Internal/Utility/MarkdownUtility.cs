1a42115f6091c5805f99badb652335795038984a
Jirrev <jirreamon@gmail.com>
2024-11-30 19:13:11 +0100

    [/] Added Documentation to the MarkdownUtility.cs

--- Internal/Utility/MarkdownUtility.cs
+++ Internal/Utility/MarkdownUtility.cs
@@ -6,6 +6,14 @@ namespace ChroniclesExporter.Utility;
 
 public static class MarkdownUtility
 {
+    /// <summary>
+    /// Attempts to parse Notion Style Relationship Links to usable Guids
+    /// </summary>
+    /// <param name="pLine">String Line of Markdown Text</param>
+    /// <param name="pKey">Field Name used as identifier</param>
+    /// <param name="pOutput">Setter which is called if the data is found</param>
+    /// <param name="pSeparator">Separator of each guid/link</param>
+    /// <returns>Whether the Parse was successful</returns>
     public static bool TryParseLinkGuids(string pLine, string pKey, Action<Guid[]> pOutput, char pSeparator = ',')
     {
         if (string.IsNullOrWhiteSpace(pLine) || !pLine.TryTrimStart(pKey, out string guidString))
@@ -13,7 +21,7 @@ public static class MarkdownUtility
 
         string[] urls = guidString.Split(pSeparator);
         if (urls.Length == 0)
-            return false;
+            return true;
 
         List<Guid> guidList = new();
         foreach (string url in urls)
@@ -27,61 +35,104 @@ public static class MarkdownUtility
         return true;
     }
 
+    /// <summary>
+    /// Attempts to register Notion Style Relationship Links to a Link Table
+    /// </summary>
+    /// <param name="pLine">String Line of Markdown Text</param>
+    /// <param name="pKey">Field Name used as identifier</param>
+    /// <param name="pLinkTable">What table to write these links to</param>
+    /// <param name="pSourceId">Guid of the source of the link</param>
+    /// <returns>Whether the registration was successful</returns>
     public static bool TryRegisterLinks(string pLine, string pKey, ELink pLinkTable, Guid pSourceId)
     {
         return TryParseLinkGuids(pLine, pKey, pGuids =>
-                {
-                    foreach (Guid guid in pGuids)
-                    {
-                        TableHandler.RegisterLink(pLinkTable, new Link(pSourceId, guid));
-                    }
-                });
+        {
+            foreach (Guid guid in pGuids)
+            {
+                TableHandler.RegisterLink(pLinkTable, new Link(pSourceId, guid));
+            }
+        });
     }
 
-    public static bool TryParseString(string pLine, string pKey, Action<String> pOutput, char pSeparator = ',')
+    /// <summary>
+    /// Attempts to parse a field string to a data setter
+    /// </summary>
+    /// <param name="pLine">String Line of Markdown Text</param>
+    /// <param name="pKey">Field Name used as identifier</param>
+    /// <param name="pOutput">Setter which is called if the data is found</param>
+    /// <returns>Whether the Parse was successful</returns>
+    public static bool TryParseString(string pLine, string pKey, Action<string> pOutput)
     {
-        if (!pLine.TryTrimStart(pLine, out string pResult)) 
+        if (!pLine.TryTrimStart(pKey, out string pResult))
             return false;
-        
+
         pOutput(pResult);
         return true;
     }
     
-    public static bool TryParseNumber<T>(string pLine, string pKey, Action<T> pOutput, 
-        System.Globalization.NumberStyles pStyles = System.Globalization.NumberStyles.Integer, 
+    /// <summary>
+    /// Attempts to parse a field number to a data setter
+    /// </summary>
+    /// <param name="pLine">String Line of Markdown Text</param>
+    /// <param name="pKey">Field Name used as identifier</param>
+    /// <param name="pOutput">Setter which is called if the data is found</param>
+    /// <param name="pStyles">NumberStyle to use (defaults to Integer)</param>
+    /// <param name="pFormat">NumberFormatInfo to use (defaults to InvariantCulture.NumberFormat)</param>
+    /// <typeparam name="T">Type to parse inherited from INumberBase</typeparam>
+    /// <returns>Whether the Parse was successful</returns>
+    public static bool TryParseNumber<T>(string pLine, string pKey, Action<T> pOutput,
+        System.Globalization.NumberStyles pStyles = System.Globalization.NumberStyles.Integer,
         System.Globalization.NumberFormatInfo? pFormat = null)
         where T : INumberBase<T>
     {
         if (!pLine.TryTrimStart(pKey, out string pStringValue) ||
             !T.TryParse(pStringValue, pStyles,
                 pFormat ?? System.Globalization.CultureInfo.InvariantCulture.NumberFormat, out T? result)) return false;
-        
+
         pOutput(result);
         return true;
     }
 
+    /// <summary>
+    /// Attempts to parse a field enum to a data setter
+    /// </summary>
+    /// <param name="pLine">String Line of Markdown Text</param>
+    /// <param name="pKey">Field Name used as identifier</param>
+    /// <param name="pOutput">Setter which is called if the data is found</param>
+    /// <typeparam name="T">Type to parse inherited from Enum</typeparam>
+    /// <returns>Whether the Parse was successful</returns>
     public static bool TryParseEnum<T>(string pLine, string pKey, Action<T> pOutput)
         where T : struct, Enum
     {
         if (!pLine.TryTrimStart(pKey, out string pEnumString) ||
             !Enum.TryParse(pEnumString.Trim(), true, out T result)) return false;
-        
+
         pOutput(result);
         return true;
     }
-    
-    public static bool TryParseEnumArray<T>(string pLine, string pKey, Action<T[]> pOutput, char pSeparator = ',') 
+
+    /// <summary>
+    /// Attempts to parse a field of multiple enums to a data setter
+    /// </summary>
+    /// <param name="pLine">String Line of Markdown Text</param>
+    /// <param name="pKey">Field Name used as identifier</param>
+    /// <param name="pOutput">Setter which is called if the data is found</param>
+    /// <param name="pSeparator">Separator of each entry</param>
+    /// <typeparam name="T">Type to parse inherited from Enum</typeparam>
+    /// <returns>Whether the Parse was successful</returns>
+    public static bool TryParseEnumArray<T>(string pLine, string pKey, Action<T[]> pOutput, char pSeparator = ',')
         where T : struct, Enum
     {
         if (!pLine.TryTrimStart(pKey, out string pSizes)) return false;
-        
-        string[] strings = pSizes.Trim().Split(',');
+
+        string[] strings = pSizes.Trim().Split(pSeparator);
         List<T> result = [];
         foreach (string s in strings)
         {
             if (Enum.TryParse(s.Trim(), true, out T sizeEnum))
                 result.Add(sizeEnum);
         }
+
         pOutput(result.ToArray());
         return true;
     }
